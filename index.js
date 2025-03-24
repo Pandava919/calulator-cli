4529fdc837a67b2824c050d120d61a1fc0b45798
Pandava919 <pandavapandu919@gmail.com>
2025-03-24 11:40:07 +0530

    argument changes in index.js, modified readme.md

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ program
 
 program.command('add')
   .description('Add numbers')
-  .argument('<number>', 'number to add seperated by ,')
-  .option('--first', 'display just the first number')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    const numbers = str.split(options.separator, limit);
+  .argument('[number...]')
+  .action((numbers) => {
     let sum = 0;
     for (let n of numbers) {
       sum += parseFloat(n);
@@ -22,12 +18,8 @@ program.command('add')
   });
 program.command('sub')
   .description('Substract the numbers')
-  .argument('<number>', 'number to substract seperated by ,')
-  .option('--first', 'display just the first number')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    const numbers = str.split(options.separator, limit);
+  .argument('[number...]')
+  .action((numbers) => {
     let sub = 0;
     for (let i = 0; i < numbers.length - 1; i++) {
       sub = parseFloat(numbers[i]) - parseFloat(numbers[i + 1])
@@ -37,12 +29,8 @@ program.command('sub')
 
 program.command('mul')
   .description('Multiply the numbers')
-  .argument('<number>', 'number to multiply seperated by ,')
-  .option('--first', 'display just the first number')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    const numbers = str.split(options.separator, limit);
+  .argument('[number...]')
+  .action((numbers) => {
     let mul = 1;
     for (let i = 0; i < numbers.length; i++) {
       mul *= parseFloat(numbers[i])
@@ -52,12 +40,8 @@ program.command('mul')
 
 program.command('div')
   .description('Division of the numbers')
-  .argument('<number>', 'number to divide seperated by ,')
-  .option('--first', 'display just the first number')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    const numbers = str.split(options.separator, limit);
+  .argument('[numbers...]')
+  .action((numbers) => {
     let division = 0;
     for (let i = 0; i < numbers.length - 1; i++) {
       division = parseFloat(numbers[i]) / parseFloat(numbers[i + 1])
@@ -67,17 +51,14 @@ program.command('div')
 
 program.command('mod')
   .description('Modulus of the numbers')
-  .argument('<number>', 'numbers to find modulus seperated by ,')
-  .option('--first', 'display just the first number')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    const numbers = str.split(options.separator, limit);
+  .argument('[numbers...]')
+  .action((numbers) => {
     let modulus = 0;
     for (let i = 0; i < numbers.length - 1; i++) {
       modulus = parseFloat(numbers[i]) % parseFloat(numbers[i + 1])
     }
     console.log(modulus);
   });
+
 
 program.parse();
